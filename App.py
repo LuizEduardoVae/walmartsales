7ee3544886ce9f3037d66d67fbf4cc1f66f9f6d9
Luiz Eduardo Vedoato Almeida Everton <luizeduardovae@MacBook-Air-de-Luiz.local>
2024-04-23 22:03:11 -0300

    fix/itensposition

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import altair as alt
 
 # Carregar os dados
 df = pd.read_csv("Output/Df_new.csv")
@@ -18,6 +19,14 @@ colors = {
     'highlight': '#E74C3C',
     'button_color': '#1ABC9C'
 }
+
+
+st.set_page_config(
+    page_title="Sales Dashboard",
+    layout="wide",
+    initial_sidebar_state='collapsed',
+    )
+
 
 # Título
 st.title('Análise de Vendas do Walmart')
@@ -45,7 +54,7 @@ fig_dept = go.Figure()
 fig_dept.add_trace(go.Scatter(x=filtered_data_dept['Date'], y=filtered_data_dept['Weekly_Sales'], mode='lines', name='Vendas'))
 fig_dept.add_trace(go.Scatter(x=filtered_data_dept['Date'], y=filtered_data_dept['Weekly_Sales'], mode='markers', name='Week', marker=dict(color='red', size=8)))
 fig_dept.update_layout(title='Vendas Médias ao Longo do Tempo', xaxis_title='Data', yaxis_title='Vendas Semanais')
-fig_dept.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
+# fig_dept.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
 
 # Seção: Volume Médio de Vendas por Loja
 fig_volume_vendas_loja = go.Figure()
@@ -58,10 +67,10 @@ else:
 media_vendas = avg_sales_per_store['Weekly_Sales'].mean()
 fig_volume_vendas_loja.add_hline(y=media_vendas, line_dash='dash', line_color='red', annotation_text=f'Média: {media_vendas:.2f}', annotation_position='bottom right', annotation_font_color='white')
 fig_volume_vendas_loja.update_layout(title_text='Volume Médio de Vendas por Loja', xaxis_title='Numero da Loja', yaxis_title='Volume Médio de Vendas')
-fig_volume_vendas_loja.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
+# fig_volume_vendas_loja.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
 
 # Organizando os gráficos em colunas
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(2, gap='medium')
 with col1:
     if selected_dept != 'Média Geral' or selected_store != 'Média Geral':
         st.plotly_chart(fig_dept, use_container_width=True)
@@ -71,7 +80,7 @@ with col1:
         fig_avg_sales_per_store_department.add_trace(go.Scatter(x=avg_sales_per_store_department['Date'], y=avg_sales_per_store_department['Weekly_Sales'], mode='lines', name='venda' ))
         fig_avg_sales_per_store_department.add_trace(go.Scatter(x=avg_sales_per_store_department['Date'], y=avg_sales_per_store_department['Weekly_Sales'], mode='markers', name='Week', marker=dict(color='red', size=8)))
         fig_avg_sales_per_store_department.update_layout(title='Vendas Médias ao Longo do Tempo', xaxis_title='Data', yaxis_title='Vendas Semanais')
-        fig_avg_sales_per_store_department.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
+        # fig_avg_sales_per_store_department.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
         st.plotly_chart(fig_avg_sales_per_store_department, use_container_width=True)
 
 with col2:
@@ -109,6 +118,6 @@ elif selected_store != 'Média Geral':
     sorted_data = sorted_data[sorted_data['Store'] == selected_store]
 
 st.title('Lojas que ficaram acima da média')
-st.dataframe(sorted_data.reset_index(drop=True), width=1200)
+st.dataframe(sorted_data.reset_index(drop=True), use_container_width=True)
 
 
